732c34c91343fbc45450742bc9fbe60080501e46
guni0527 <kkh02527@naver.com>
2025-05-23 14:51:09 +0900

    제출쓰
    제출

---
+++ Assets/Script/Item/ItemObject.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public interface IInteractable
+{
+    public string GetInteractPrompt();
+    public void OnInteract();
+}
+
+public class ItemObject : MonoBehaviour, IInteractable
+{
+    public ItemData data;
+
+    public string GetInteractPrompt()
+    {
+        string str = $"{data.displayName}\n{data.description}";
+        Debug.Log(data);
+        return str;
+    }
+
+    public void OnInteract()
+    {
+        //Player 스크립트 먼저 수정
+        CharacterManager.Instance.Player.itemData = data;
+        CharacterManager.Instance.Player.addItem?.Invoke();
+        Destroy(gameObject);
+    }
+}
